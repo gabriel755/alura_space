30c7c7390aa6b9c0490d1a2f4ea8840bdc79656f
gabriel755 <gabriel.fiamete@icloud.com>
2023-03-15 19:35:23 -0300

    Adicionando DB

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    desc = models.TextField(null=False, blank=False)
+    image = models.CharField(max_length=100, null=False, blank=False)
+    
+    def __str__(self):
+        return f"Fotografia [nome=(self.nome)]"
+    
--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+
 def index(req):
-    return render(req, 'galeria/index.html')
+    data = {
+        1: {"nome": "Nebulosa de Carina",
+            "legenda": "webbtelescope.org/ NASA/ James Webb"},
+        2: {"nome": "Galáxia NGC 1079", 
+            "legenda": "nasa.org / NASA / Hubble"},
+    }
+
+    return render(req, 'galeria/index.html', {"cards": data})
 
 def imagem(req):
     return render(req, 'galeria/imagem.html')
